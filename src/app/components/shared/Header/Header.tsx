ce13e90db2ed359e04aa7a48ca8ce266f65b9142
CarlosVera <veracar111@gmail.com>
2025-03-31 15:36:25 -0500

    landing page

---
+++ src/app/components/shared/Header/Header.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <Link href="/">
+                    <li>Home</li>
+                </Link>
+                <Link href="/store">
+                    <li>Store</li>
+                </Link>
+            </nav>
+        </header>
+    );
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
-import styles from "./page.module.css";
+import { Description } from "./components/home/Description";
+import { Hero } from "./components/home/Hero";
+import { MainProducts } from "./components/home/MainProducts";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Hello World</h1>
-      </main>
-      <footer className={styles.footer}>
-        
-      </footer>
-    </div>
+    <main>
+      <Hero />
+      <Description />
+      <MainProducts />
+    </main>
   );
 }
