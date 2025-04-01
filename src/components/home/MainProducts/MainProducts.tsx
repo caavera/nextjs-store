8aacf51fd9b0dfb7ad4d763128119e9842f9a401
CarlosVera <veracar111@gmail.com>
2025-04-01 17:42:54 -0500

    services arch

--- src/components/home/MainProducts/MainProducts.tsx
+++ src/components/home/MainProducts/MainProducts.tsx
@@ -1,20 +1,7 @@
 import Image from 'next/image'
 import styles from './MainProducts.module.sass'
+import { getProducts } from 'app/services/shopify'
 
-const getProducts = async () => {
-  try {
-    const response = await fetch(`${process.env.SHOPIFY_HOSTNAME}/admin/api/2025-01/products.json`, {
-      headers: new Headers({
-        'X-Shopify-Access-Token': process.env.SHOPIFY_ACCES_TOKEN || ""
-      })
-    })
-    const { products } = await response.json()
-    return products
-  } catch (error) {
-    console.error('Error fetching products:', error)
-    return []
-  }
-}
 
 export const MainProducts = async () => {
   const products = await getProducts()
---
+++ src/config/env.ts
@@ -0,0 +1,4 @@
+export const env = {
+    SHOPIFY_HOSTNAME: process.env.SHOPIFY_HOSTNAME || "",
+    SHOPIFY_ACCES_TOKEN: process.env.SHOPIFY_ACCES_TOKEN || "",
+}
