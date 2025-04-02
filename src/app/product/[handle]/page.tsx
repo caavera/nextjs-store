784d0b8973394475a92ae7a6f1064d79d150d8da
CarlosVera <veracar111@gmail.com>
2025-04-02 12:15:18 -0500

    nextjs redirect

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,6 @@
 import { ProductView } from "app/components/product/ProductView"
 import { getProduct } from "app/services/shopify/products"
+import { redirect } from "next/navigation"
 
 interface ProductPageProps {
   searchParams: {
@@ -9,7 +10,15 @@ interface ProductPageProps {
 
 export default async function ProductPage({ searchParams }: ProductPageProps) {
   const id = searchParams.id
+  if (!id) {
+    redirect('/store')
+  }
   const product = await getProduct(id)
+  
+  // Redirect to store if product is not found
+  if (!product) {
+    redirect('/store')
+  }
 
   return <ProductView product={product} />
 }
