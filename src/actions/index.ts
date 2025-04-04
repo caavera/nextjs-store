3d8502a5b23db7a88b7e9a9de5f173f12c2985c3
CarlosVera <veracar111@gmail.com>
2025-04-04 15:15:48 -0500

    adding signup action

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,5 +1,20 @@
 "use server"
+import { GraphQLClientSingleton } from "@/graphql"
+import { createUserMutation } from "@/graphql/mutations/createUserMutation"
 
-export async function handleCreateUser(formData: FormData) {
-  console.log("handleCreateUser", formData)
+export const handleCreateUser = async (formData: FormData) => {
+  const formDataObject = Object.fromEntries(formData)
+  delete formDataObject["password_confirmation"]
+  const graphqlClient = GraphQLClientSingleton.getInstance().getClient()
+  const variables = {
+    input: {
+      ...formDataObject,
+      phone: '+57' + formDataObject.phone
+    }
+  }
+
+  const { customerCreate } = await graphqlClient.request(createUserMutation, variables)
+  const { customerUserErrors, customer } = customerCreate
+  console.log(customer)
+  console.log(customerUserErrors)
 }
