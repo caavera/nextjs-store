59d7647ecad7ecef172bd1a0db838ec510d03b31
CarlosVera <veracar111@gmail.com>
2025-04-05 10:51:12 -0500

    shopping-cart styles

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,32 +1,31 @@
 import Link from 'next/link'
-import styles from './Header.module.sass'
+import { ShoppingCart } from '../ShoppingCart'
 import { validateAccessToken } from 'app/utils/auth/validateAccessToken'
+import styles from './Header.module.sass'
+
 
 export const Header = async () => {
-    const customer = await validateAccessToken()
+  const customer = await validateAccessToken()
 
-    return (
-        <header>
-            <nav>
-                <ul className={styles.Header__list}>
-                    <li>
-                        <Link href="/">
-                            Home
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/store">
-                            Store
-                        </Link>
-                    </li>
-                    <li>
-                        {customer?.firstName ? (
-                            <p>Hola! {customer.firstName}</p>
-                        ) : (
-                            <Link href="/login">Login</Link>
-                        )}
-                    </li>
-                </ul>
-            </nav>
-        </header>)
+  return (
+    <header className={styles.Header}>
+      <nav>
+        <ul className={styles.Header__list}>
+          <li>
+            <Link href="/">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href="/store">
+              Store
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.Header__user}>
+        {customer?.firstName ? (<p>Hola! {customer.firstName}</p>) : (<Link href="/login">Login</Link>)}
+        <ShoppingCart />
+      </div>
+    </header>)
 }
