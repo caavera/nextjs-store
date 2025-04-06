2c5d93df9e5d8d23924cdc0f0beecf994a66d55d
CarlosVera <veracar111@gmail.com>
2025-04-06 18:14:32 -0500

    fixing errors

--- src/app/store/[[...categories]]/page.tsx
+++ src/app/store/[[...categories]]/page.tsx
@@ -9,13 +9,20 @@ interface CategoryProps {
   searchParams?: Promise<Record<string, string>>
 }
 
+// Define interface for the collection object based on what getCollections returns
+interface Collection {
+  id: string;
+  title: string;
+  handle: string;
+}
+
 export default async function Category(props: CategoryProps){
   const { categories } = props.params
   let products = []
   const collections = await getCollections()
   
   if (categories?.length > 0) {
-    const selectedCollectionId = collections.find((collection) => collection.handle === categories[0]).id
+    const selectedCollectionId = collections.find((collection: Collection) => collection.handle === categories[0]).id
     products = await getCollectionProducts(selectedCollectionId)
   }else {
     products = await getProducts()
